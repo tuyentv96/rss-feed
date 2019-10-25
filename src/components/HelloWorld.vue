e7240b389a2bbbcc1312a41c3092bca72c36f17a
tuyentv96 <vat1906@gmail.com>
2019-10-25 18:24:03 +0700

    inital

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,64 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <el-row>
+      <el-button type="danger" @click="editvalue()">Danger</el-button>
+    </el-row>
   </div>
 </template>
 
 <script>
+import firebase from "firebase";
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCruMO0xdKBm7OV6oDiw7cfbP4w0YjEeKQ",
+  authDomain: "rss-reader-ea442.firebaseapp.com",
+  databaseURL: "https://rss-reader-ea442.firebaseio.com",
+  projectId: "rss-reader-ea442",
+  storageBucket: "rss-reader-ea442.appspot.com",
+  messagingSenderId: "591950448463",
+  appId: "1:591950448463:web:db9e35b3a159433531a73a",
+  measurementId: "G-WNCMR5WGQE"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+let urls = [];
+let Parser = require("rss-parser");
+let parser = new Parser({
+  headers: {
+    mode: "cors"
+  }
+});
+
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
     msg: String
+  },
+  methods: {
+    editvalue() {
+      console.log("clicked");
+      var ref = firebase.database().ref("rss/urls");
+      ref.on("value", function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+          var childData = childSnapshot.val();
+          urls.push(childData);
+        });
+
+        console.log(urls);
+        (async () => {
+          let feed = await parser.parseURL("https://quan-cam.com/rss");
+          console.log(feed.title);
+
+          feed.items.forEach(item => {
+            console.log(item.title + ":" + item.link);
+          });
+        })();
+      });
+    }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
