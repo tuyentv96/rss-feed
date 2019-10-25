43c46e16691107881b1abf5cc44a80f9b30d6a0c
tuyentv96 <vat1906@gmail.com>
2019-10-25 19:57:45 +0700

    add href

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="hello">
-    <el-row>
-      <el-button type="danger" @click="editvalue()">Danger</el-button>
-    </el-row>
+    <div v-for="item in articles" v-bind:key="item.id">
+      <h3>{{ item }}</h3>
+      <a v-bind:href="item.link">{{item.title}}</a>
+      <el-divider></el-divider>
+    </div>
   </div>
 </template>
 
 <script>
 import firebase from "firebase";
+// import axios from "axios";
 // Your web app's Firebase configuration
 var firebaseConfig = {
   apiKey: "AIzaSyCruMO0xdKBm7OV6oDiw7cfbP4w0YjEeKQ",
@@ -22,41 +25,51 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
-
 let urls = [];
 let Parser = require("rss-parser");
-let parser = new Parser({
-  headers: {
-    mode: "cors"
-  }
-});
+let parser = new Parser({});
+let articles = [];
 
+function fetchData() {
+  console.log("clicked");
+  var ref = firebase.database().ref("rss/urls");
+  ref.on("value", function(snapshot) {
+    snapshot.forEach(function(childSnapshot) {
+      var childData = childSnapshot.val();
+      urls.push(childData);
+      console.log(childData);
+      (async () => {
+        const data = await fetch(
+          "https://tuyentv-cors.herokuapp.com/" + childData,
+          {
+            mode: "cors"
+          }
+        );
+        const text = await data.text();
+        parser.parseString(text, (err, parsed) => {
+          parsed.items.forEach(function(entry) {
+            console.log(entry.title + ":" + entry.link);
+            articles.push({
+              title: entry.title,
+              link: entry.link
+            });
+          });
+        });
+      })();
+    });
+  });
+}
+
+fetchData();
 export default {
   name: "HelloWorld",
   props: {
     msg: String
   },
-  methods: {
-    editvalue() {
-      console.log("clicked");
-      var ref = firebase.database().ref("rss/urls");
-      ref.on("value", function(snapshot) {
-        snapshot.forEach(function(childSnapshot) {
-          var childData = childSnapshot.val();
-          urls.push(childData);
-        });
-
-        console.log(urls);
-        (async () => {
-          let feed = await parser.parseURL("https://quan-cam.com/rss");
-          console.log(feed.title);
-
-          feed.items.forEach(item => {
-            console.log(item.title + ":" + item.link);
-          });
-        })();
-      });
-    }
+  data() {
+    return {
+      articles: articles
+    };
   }
 };
 </script>
